c4f21c8e3ba2b43a01d4465689346501371191c6
Faustharus <chailloleau-damien@outlook.fr>
2024-08-05 18:09:30 +0200

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Damien Chailloleau on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
